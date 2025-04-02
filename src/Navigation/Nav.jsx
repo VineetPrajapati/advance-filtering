823c53df60715037b6bef61eb87b77446d2342a5
Vineet <infinite2202@gmail.com>
2025-04-02 15:21:58 +0530

    Nav.jsx & Nav.css

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,3 +1,6 @@
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+
 import "./Nav.css";
 const Nav = () => {
   return (
@@ -10,7 +13,15 @@ const Nav = () => {
         />
       </div>
       <div className="profile-container">
-        
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
       </div>
     </nav>
   );
