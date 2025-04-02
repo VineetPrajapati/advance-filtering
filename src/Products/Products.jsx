f4c617a327afb33e29d9782a6192aa769f5bef31
Vineet <infinite2202@gmail.com>
2025-04-02 15:46:06 +0530

    products

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,9 +1,40 @@
-import "./Products.css"
+import { AiFillStar } from "react-icons/ai";
+import "./Products.css";
+import { BsFillBagHeartFill } from "react-icons/bs";
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <>
+      <section className="card-container">
+        <section className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/719gdz8lsTS._AC_UX575_.jpg"
+            alt="Shoes"
+            className="card-img"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoe</h3>
+            <section className="card-reviews">
+              <AiFillStar className="rating-start" />{" "}
+              <AiFillStar className="rating-start" />
+              <AiFillStar className="rating-start" />
+              <AiFillStar className="rating-start" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>$300</del>
+                200
+              </div>
+              <div className="bag">
+                <BsFillBagHeartFill className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+};
 
-export default Products
+export default Products;
