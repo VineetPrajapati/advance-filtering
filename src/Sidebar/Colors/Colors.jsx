d6e6014912234e846f15ecb18b6214721d57786e
Vineet <infinite2202@gmail.com>
2025-04-03 21:52:48 +0530

    colors.jsx

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -1,16 +1,55 @@
 import Input from "../../components/Input";
 import "./Colors.css";
 
-const Colors = () => {
+const Colors = ({ handleChange }) => {
   return (
     <div>
       <h2 className="sidebar-title color-title">Colors</h2>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+      <label className="sidebar-label-container">
+        <input onChange={handleChange} type="radio" value="" name="test" />
+        <span className="checkmark all"></span>All
+      </label>
+      <Input
+        handleChange={handleChange}
+        value="black"
+        title="Black"
+        name="test1"
+        color="black"
+      />
+      <Input
+        handleChange={handleChange}
+        value="blue"
+        title="Black"
+        name="test1"
+        color="blue"
+      />
+      <Input
+        handleChange={handleChange}
+        value="red"
+        title="Red"
+        name="test1"
+        color="red"
+      />
+      <Input
+        handleChange={handleChange}
+        value="green"
+        title="Green"
+        name="test1"
+        color="green"
+      />
+
+      <label className="sidebar-label-container">
+        <input
+          type="radio"
+          onChange={handleChange}
+          value="white"
+          name="test1"
+        />
+        <span
+          className="checkmark"
+          style={{ background: "white", border: "2px solid black" }}
+        ></span>White
+      </label>
     </div>
   );
 };
