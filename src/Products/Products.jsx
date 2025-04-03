a78cc3924ee2278f0f6cc09c885a4d6e2635755e
Vineet <infinite2202@gmail.com>
2025-04-03 20:21:05 +0530

    card refactorted

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,37 +1,22 @@
-import { AiFillStar } from "react-icons/ai";
 import "./Products.css";
-import { BsFillBagHeartFill } from "react-icons/bs";
+
+import Card from "../components/Card";
 
 const Products = () => {
   return (
     <>
       <section className="card-container">
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/719gdz8lsTS._AC_UX575_.jpg"
-            alt="Shoes"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-reviews">
-              <AiFillStar className="rating-start" />{" "}
-              <AiFillStar className="rating-start" />
-              <AiFillStar className="rating-start" />
-              <AiFillStar className="rating-start" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>
-                200
-              </div>
-              <div className="bag">
-                <BsFillBagHeartFill className="bag-icon" />
-              </div>
-            </section>
-          </div>
-        </section>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </section>
     </>
   );
